4ed1a6f1641a12855aa44ae529ed466926099505
Rishitha8 <82807753+Rishitha8@users.noreply.github.com>
2021-06-10 15:41:17 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    // this makes the bird fly and empty the slingShot. 
+     fly(){
+     this.sling.bodyA = null;
+ }
+    display(){
+     if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+  } 
+}
